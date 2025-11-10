8a3fa064c542d3a8db8ee1bc200401cc1a45f173
doiasahi <CA01244018@st.kawahara.ac.jp>
2025-11-10 11:28:05 +0900

    一旦のステートクラスのpush

--- Assets/C#Scripts/EnemyFolder/EnemyStateMachineScript.cs
+++ Assets/C#Scripts/EnemyFolder/EnemyStateMachineScript.cs
@@ -1,24 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.XR;
+
+//定数クラス
+public static class CommonConst
+{
+
+}
 
 //エネミーのステート
 public enum EnemyState
 {
-    Idle,
+    Patrol,
     Chase,
     Attack,
     Dead
 }
 
+
+[RequireComponent(typeof(EnemyBaseScript))]
 public class EnemyStateMachineScript : MonoBehaviour
 {
-    public EnemyState currentState;
-    public Transform player;
-    public float chaseDistance = 0;
-    public float attackDistance = 0;
-    
+    [Header("ターゲット")]
+    [SerializeField] Transform player;
+
+    [Header("距離しきい値")]
+    [SerializeField,Tooltip("この距離内で追跡開始")]
+    public float chaseDistance = 15.0f;
+    [SerializeField,Tooltip("この距離内で攻撃開始")]
+    float attackDistance = 2.5f;
+    [SerializeField, Tooltip("攻撃のクールダウン")]
+    float attackInterval = 1.0f;
+
+    [Header("移動")]
+    [SerializeField,Tooltip("NavMashAgentを使って追跡ができる(任意)")]
+    bool usNavMesh = true;
+    [SerializeField] float stopDistance = 1.0f;
+
+    [Header("参照(任意)")]
+    [SerializeField] EnemyMove move;
+    [SerializeField] EnemyAttacks attacks;
+    [SerializeField] NavMeshAgent agent;
+
+    EnemyBaseScript E_Base;
+    public EnemyState currentState { get; private set; } =EnemyState.Patrol;
+    float _attackTimer = 0.0f;
     // Start is called before the first frame update
+    void Awake()
+    {
+       E_Base = GetComponent<EnemyBaseScript>();
+       if(!move) move = GetComponent<EnemyMove>();
+       if(!attacks) attacks = GetComponent<EnemyAttacks>();
+       if(!agent) agent = GetComponent<NavMeshAgent>();
+
+        //死亡したらDeadへ
+        E_Base.OnDeath += () => ChangeState(EnemyState.Dead);
+    }
     void Start()
     {
         
@@ -28,5 +68,62 @@ public class EnemyStateMachineScript : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void ChangeState(EnemyState nextState)
+    {
+        if(currentState == nextState) 
+        {
+            return;
+        }
+
+        //Exit
+        OnExit(currentState);
+
+        //状態遷移
+        currentState = nextState;
+
+        //Enter
+        OnEnter(currentState);
+    }
+
+    void OnEnter(EnemyState state)
+    {
+        switch(state)
+        {
+            case EnemyState.Patrol:
+                if (usNavMesh && agent) { agent.isStopped = true;agent.ResetPath();}
+                break;
+            case EnemyState.Chase:
+                break;
+            case EnemyState.Attack:
+                _attackTimer = 0f;
+                break;
+        }
+    }
+
+    private void OnExit(EnemyState state)
+    {
+        
+    }
+
+    void TrickIdle(float distSq)
+    {
+        if (!HasPlayer()) return;
+    }
+
+    float DistanceSqToPlayer()
+    {
+        if (!HasPlayer()) return float.MaxValue;
+        return (player.position - transform.position).sqrMagnitude;
+    }
+
+    bool HasPlayer() => player != null;
+
+    public void SetPlayer(Transform t) =>player = t;
+    public void SetDistances(float chase, float attack)
+    {
+        chaseDistance = Mathf.Max(0, chaseDistance);
+        attackDistance = Mathf.Clamp(attack,0.1f, attackDistance);
     }
 }
